3c85c457b1de991300ce37b5a6e9a67731b85980
Cullen S <cullensmi@gmail.com>
2020-11-11 12:10:13 -0500

    Finished Lambda Expressions chapter

--- README.md
+++ README.md
@@ -4,10 +4,55 @@ Comparable and comparator interface are both functional interfaces.
 
 Lambda expression - anonymous function we can pass around.
 
-Lamdda expressions are essentially objects that can be used to represent anonymous functions.
+Lambda expressions are essentially objects that can be used to represent anonymous functions.
 
 In lambda expressions, the keyword this references the enclosing object whereas in anonymous inner classes, this 
 references the current instance of the anonymous inner class and other difference between these two
 types is that the anonymous inner classes can have state so, they can have field to store some data in. 
 In lambda expressions we cannot have fields.
+
+Method references allow lambda expressions to be more compact and easier to read.
+
+Java.util.function is the package that has predefined functional interfaces.
+
+There are four types of functional interfaces in Java: consumer, supplier, function and predicate.
+
+Consumer interface - represents an operation that takes a single argument and returns no result. The print method in this package is similar to this.
+void consume(obj)
+
+BiConsumer and IntConsumer are commonly used.
+
+Supplier interface is the opposite of the consumer interface, it represents an operation that takes no input and returns a value. It supplies a value.
+
+Supplier interfaces also have other interfaces that work with primitive values.
+
+obj supply()
+
+Function interface represents a function that can map a value to a different value.
+
+It represents an operation that returns a value. T(type of argument) and R(type of the result).
+
+Function interfaces have four methods, one of which is abstract.
+
+BiFunction<T, U, R> takes two arguments and returns a result.
+
+IntFunction represents a function that takes a primitive integer. There is LongFunction, DoubleFunction.
+
+ToIntFunction has its argument parameterised, and the return is a primitive.
+
+IntToLong takes a primitive integer and returns a primitive long.
+
+Predicate interface - takes an object and checks to see if the object satisfies some criteria. It is used to filter data. 
+
+Predicate helps filter data. Has a single abstract method of test.
+
+BiPredicate takes two arguments and returns a boolean.
+
+IntPredicate, LongPredicate, etc.
+
+BinaryOperator interface is a special type of function, there are primitive specializations of the interface too.
+
+
+
+
 
--- src/com/csmithswim/LambdasDemo.java
+++ src/com/csmithswim/LambdasDemo.java
@@ -1,39 +1,167 @@
 package com.csmithswim;
 
+import java.util.List;
+import java.util.function.*;
+
 public class LambdasDemo {
-    public String prefix = "-";
+    //UnaryOperator Interface
+    public static void show() {
+        UnaryOperator<Integer> square = n -> n * n;
+        UnaryOperator<Integer> increment = n -> n +1;
 
-    //Can access the variable as a static field
-    public static String prefix = "-";
-
-    public  void show() {
-        //Can access local variables
-        String prefix = "-";
-
-        greet(message -> System.out.println(prefix + message));
-
-        //Lambda expression, we pass the implementation of the interface method as an anonymous function
-        // -> Is the lambda operator, {} is the body of the function
-        //No need to put the datatype in the parameter, parentheses can be removed when there is just one parameter and no need for
-        // {} if there is just one line in the body
-        greet(message -> System.out.println(message));
-
-        //Lambda expressions can be stored in a variable
-        Printer printer = message -> System.out.println(message);
-
-        //instance of an anonymous inter-class, it has no name and is being used inside a method
-        greet(new Printer() {
-            @Override
-            public void print(String message) {
-
-            }
-        });
+        var result = increment.andThen(square).apply(1);
+        System.out.println(result);
     }
 
-    public static void greet(Printer printer){
-        printer.print("Hello World");
-    }
+
+//    //BinaryOperator Interface
+//    public static void show() {
+//        //a, b -> a + b -> square
+//        BinaryOperator<Integer> add = (a, b) -> a + b;
+//        Function<Integer, Integer> square = a -> a * a;
+//        var result = add.andThen(square).apply(1,2);
+//
+//
+//
+//        //More efficient to use the IntBinaryOperator
+//        BinaryOperator<Integer> add = (a, b) -> a + b;
+//        var result = add.apply(1,2);
+//    }
 }
+
+//    //Combining Predicate interfaces
+//    public static void show() {
+//        Predicate<String> hasLeftBrace = str -> str.startsWith("{");
+//        Predicate<String> hasRightBrace = str -> str.startsWith("}");
+//
+//    Predicate<String> hasLeftAndRightBraces = hasLeftBrace.and(hasRightBrace);
+//    var result=  hasLeftAndRightBraces.test("{key:value}");
+//
+//    //Negate method does the opposite of what the predicate method does
+//    hasLeftBrace.negate();
+//    }
+//}
+
+//    //Predicate interface
+//    public static void show() {
+//        Predicate<String> isLongerThan5 = str -> str.length() >5;
+//        isLongerThan5.test("sky");
+//    }
+//}
+
+
+//    //Composing Functions
+//    public static void show() {
+//    //"key:value"
+//    //first: "key=value"
+//    //second: "{key=value}"
+//    Function<String, String> replaceColon = str -> str.replace(":", "=");
+//    Function<String, String> addBraces = str -> "{" + str + "}";
+//
+//    //There are two ways to compose functions. Declarative Programming
+//    var result = replaceColon
+//                            .andThen(addBraces)
+//                            .apply("key:value");
+//
+//    //The compose method is similar to andThen but chains them in the reverse order.
+//    result = addBraces.compose(replaceColon).apply("key:value");
+//
+//    System.out.println(result);
+//    }
+//}
+
+
+//Example of function interface
+//    public static void show() {
+//        Function<String, Integer> map = str -> str.length();
+//        var length = map.apply("sky");
+//        System.out.println(length);
+//    }
+//}
+
+
+    //Examples of supplier interfaces
+//    public static void show() {
+//        Supplier<Double> getRandom = () -> Math.random();
+//        //The Math.random() function is not run until we call it, something known as lazy evaluation
+//        var random = getRandom.get();
+//        System.out.println(random);
+//    }
+//}
+
+    //Examples of the consumer interface
+//    public static void show() {
+//        List<String> list = List.of("a", "b", "c");
+//        Consumer<String> print = item -> System.out.println(item);
+//        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
+//
+//        list.forEach(print.andThen(printUpperCase));
+//    }
+//}
+//        List<Integer> list = List.of(1, 2, 3);
+//
+//        //One way to iterate, type of imperative programming (for, if/else, switch/case), i.e. how stuff needs to be done
+//        for (var item : list)
+//            System.out.println(item);
+//
+//        //Using a lambda expression to iterate through, an example of declarative programming, i.e. what needs to be done
+//        list.forEach(item -> System.out.println(item));
+//}
+
+    //Using constructor
+//    public LambdasDemo(String message){
+//    }
+//
+//    public static void show() {
+//        greet(message -> new LambdasDemo(message));
+//        greet(LambdasDemo::new);
+//    }
+//
+//    public static void greet(Printer printer){
+//        printer.print("Hello World");
+//    }
+//
+//
+
+    //Using instance method
+//    public void void print(String message){}
+//
+//    public  void show() {
+//   var demo = new LambdasDemo();
+//   greet(message -> demo.print(message));
+//   greet(demo::print);
+//    }
+//
+//    public static void greet(Printer printer){
+//        printer.print("Hello World");
+//    }
+
+    //Using static method
+//    public static void print(String message){}
+//
+//    public  void show() {
+//        greet(message -> print(message));
+//        greet(LambdasDemo::print);
+//    }
+//
+//    public static void greet(Printer printer){
+//        printer.print("Hello World");
+//    }
+}
+
+//    greet(message -> System.out.println(message));
+//
+//        //Sometimes it's easier to reference the method directly
+//        // Class/Object::method
+//        //IntelliJ can convert it to method reference by highlighting and doing alt+enter
+//        //We can reference static or instance methods and constructors in a class
+//        greet(System.out::println);
+//    }
+//
+//    public static void greet(Printer printer){
+//        printer.print("Hello World");
+//    }
+//}
 
 //public class LambdasDemo {
 //    public String prefix = "-";
